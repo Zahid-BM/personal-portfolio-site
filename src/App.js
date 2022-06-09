67a03bdd4d11558c08e48f5325894d73128019dd
Zahid-BM <bmzahidulislam@outlook.com>
2022-06-09 23:24:12 +0600

    Alhamdulillah trying animation. Still failure. Now netlify deployment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
 
+
 function App() {
   return (
     <div className='font-link'>
--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -7,15 +7,20 @@ import github from '../../resources/images/icons/github.png';
 import linkedin from '../../resources/images/icons/linkedin.png';
 import whatsapp from '../../resources/images/icons/whatsapp.png';
 import email from '../../resources/images/icons/email.png';
+import TypingTextAnimation from 'react-typing-text-animation';
 
 const Banner = () => {
+
+
     return (
         <div className='bg-banner'>
             <Header></Header>
             <Container style={{ backgroundImage: `url(${imgBg})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
                 <Row>
                     <Col className='my-auto' lg={6}>
-                        <h1 className='fw-bolder'>Hello, I Am <br /> <span className='fw-bolder display-2 title'>BM. Zahidul Islam</span></h1>
+                        <h1 className='fw-bolder'>Hello, I Am <br /> <span className='fw-bolder display-2 title '>
+                            <TypingTextAnimation reset={true}  text='BM. Zahidul Islam' />
+                        </span></h1>
                         <p className='text-secondary fw-bold'>A Full-stack Web Developer with full of dedication and professionalism. Skills, coping with new technologies and thirst for gathering knowledge make me a confident Web Developer.</p>
                         <Button variant='outline-success' className='px-5 py-3 rounded-pill fw-bold hover1 mb-4'>Download Resume</Button>
                         <br />
