deef15ed0b9a0c64bc6974329e54972117886738
Zahid-BM <bmzahidulislam@outlook.com>
2022-06-09 12:52:58 +0600

    Alhmdulillah all font changed

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -3,38 +3,45 @@ import { Button, Container, Nav, Navbar, NavLink } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../../images/Logo-1.png'
 
+
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg">
             <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        src={logo}
-                        className="d-inline-block align-top fw-bolder"
-                        alt="React Bootstrap logo"
-                    />
+                <Navbar.Brand>
+                    <LinkContainer to={'home'}>
+                        <NavLink>
+                            <img
+                                src={logo}
+                                className="d-inline-block align-top fw-bolder my-0"
+                                alt="React Bootstrap logo"
+                            />
+                        </NavLink>
+                    </LinkContainer>
+
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto ">
                         <LinkContainer to='home'>
-                            <NavLink className='me-5 btn px-3'>
+                            <NavLink className='me-5 btn-sm btn-outline-warning rounded-pill px-4'>
                                 Home
                             </NavLink>
                         </LinkContainer>
                         <LinkContainer to='about'>
-                            <NavLink className='me-5 btn px-3'>
+                            <NavLink className='me-5 btn-sm btn-outline-warning rounded-pill px-4'>
                                 About
                             </NavLink>
                         </LinkContainer>
                         <LinkContainer to='portfolio'>
-                            <NavLink className='me-5 btn px-3'>
+                            <NavLink className='me-5 btn-sm btn-outline-warning rounded-pill px-4'>
                                 Portfolio
                             </NavLink>
                         </LinkContainer>
                     </Nav>
+                
                     <Nav>
-                        <Button variant='warning' size='lg' className='rounded-pill px-5'>
+                        <Button variant='outline-warning' size='md' className='rounded-pill px-5'>
                             Hire Me
                         </Button>
                     </Nav>
