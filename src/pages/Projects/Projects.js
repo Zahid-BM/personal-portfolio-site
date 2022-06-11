a96342d0a61bb552ec2d2da8558d4dd5a032a14b
Zahid-BM <bmzahidulislam@outlook.com>
2022-06-11 16:05:55 +0600

    Alhamdulillah all projects shown with button

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import SingleProject from './SingleProject';
+import Rotate from 'react-reveal/Rotate';
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
@@ -14,11 +15,14 @@ const Projects = () => {
             <Container className='my-5'>
                 <h3 className='text-center text-secondary'>See my recent projects</h3>
                 <h1 className='text-center display-1 fw-bolder text-warning'>Below are the most recent </h1>
+                <Rotate bottom left>
                 <Row xs={1} md={2} className="g-4">
-                    {
-                        projects.map(project => <SingleProject key={project.id} project={project}></SingleProject>)
-                    }
+                
+                        {
+                            projects.map(project => <SingleProject key={project.id} project={project}></SingleProject>)
+                        }
                 </Row>
+                 </Rotate>
             </Container>
 
         </>
--- src/pages/Projects/SingleProject.js
+++ src/pages/Projects/SingleProject.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Button, Card, Col } from 'react-bootstrap';
+
 
 const SingleProject = ({ project }) => {
-    const { name, id, img1, img2, img3, img4, about, description1, description2, description3, description4, description5 } = project;
+    const { name, id, img1, about, technology } = project;
     return (
+
         <Col>
-            <Card className='border-0 h-100 shadow'>
-                <Card.Img className='h-100 w-100' variant="top" src={img1} />
-                <Card.Body className='bg-box'>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        {about}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
+                <Card className='border-0 h-100 shadow'>
+                    <Card.Img className='' variant="top" src={img1} />
+                    <Card.Body className='bg-box d-flex flex-column'>
+                        <Card.Title className='fw-bolder text-warning'>{name}</Card.Title>
+                        <Card.Text className='text-white'>
+                            {about}
+                        </Card.Text>
+                        <Card.Subtitle className='fw-bold text-warning'>
+                            Built with :
+                        </Card.Subtitle>
+                        <Card.Text className='text-white'>
+                            {technology}
+                        </Card.Text>
+                        <div className="d-flex justify-content-evenly mt-auto">
+                            <div>
+                                <Button variant='warning' className='px-4 rounded-pill text-white hover1'>Visit Site</Button>
+                            </div>
+                            <div>
+                                <Button variant='warning' className='px-4 rounded-pill text-white hover1'>Details info</Button>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+
     );
 };
 
