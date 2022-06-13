468c60de8f010d297835ac11c044cd8b4a6f4ea7
Zahid-BM <bmzahidulislam@outlook.com>
2022-06-14 03:51:20 +0600

    Alhamdulillah skills icon and structures updated

--- src/pages/Home/Skills.js
+++ src/pages/Home/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import javascript from '../../resources/images/icons/javascript.png';
 import reactjs from '../../resources/images/icons/reactjs.png';
 import nodejs from '../../resources/images/icons/nodejs.png';
@@ -14,25 +14,39 @@ const Skills = () => {
         <>
 
             <Container fluid className='bg-box2 p-5'>
-
-                <div>
-                    <h3 className='text-center text-dark'>My expertise and skills</h3>
-                    <h1 className='text-center display-1 fw-bolder text-secondary'>Love to work in </h1>
-                    <hr />
-                    <Bounce bottom>
-                        <div className='d-flex justify-content-around align-items-center my-5'>
-                            <Pulse forever="true">
-                                <div><img className='w-100' src={javascript} alt="" /></div>
-                                <div><img className='w-100' src={reactjs} alt="" /></div>
-                                <div><img className='w-100' src={nodejs} alt="" /></div>
-                                <div><img className='w-100' src={express} alt="" /></div>
-                                <div><img className='w-100' src={mongodb} alt="" /></div>
-                                <div><img className='w-100' src={bootstrap} alt="" /></div>
-                            </Pulse>
-                        </div>
-                    </Bounce>
-                </div>
-
+                <h3 className='text-center text-dark'>My expertise and skills</h3>
+                <h1 className='text-center display-1 fw-bolder text-secondary'>Love to work in </h1>
+                <hr />
+                <Bounce bottom>
+                    <Row xs={1} md={4} className='g-4 my-4'>
+                        <Pulse duration={4000} forever="true">
+                            <Col className='text-center'>
+                                <img className='w-100 ' src={javascript} alt="" /><span className='fw-bolder fs-2 '>JavaScript</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={reactjs} alt="" /> <span className='fw-bolder fs-2 '>Reactjs</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={nodejs} alt="" /> <span className='fw-bolder fs-2 '>Nodejs</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={express} alt="" /> <span className='fw-bolder fs-2 '>Expressjs</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={mongodb} alt="" /><span className='fw-bolder fs-2 '>MongoDB</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={bootstrap} alt="" /> <span className='fw-bolder fs-2 '>Bootstrap5</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={bootstrap} alt="" /> <span className='fw-bolder fs-2 '>HTML5</span>
+                            </Col>
+                            <Col className='text-center'>
+                                <img className='w-100' src={bootstrap} alt="" /> <span className='fw-bolder fs-2 '>CSS3</span>
+                            </Col>
+                        </Pulse>
+                    </Row>
+                </Bounce>
             </Container>
 
         </>
